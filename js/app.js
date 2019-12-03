cd76392916273bbdac2d00ceb26d9e6dc1212f1a
MichAngel <mangelette@gmail.com>
2019-12-03 13:07:07 -0800

    Event Listeners Update

--- js/app.js
+++ js/app.js
@@ -1,80 +1,54 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-
-/**
- * Define Global Variables
- *
- */
-
 const navbar = document.getElementById("navbar__list");
 const navbar__head = document.getElementById("navbar__menu");
 const sections = document.querySelectorAll("section");
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
-*/
-
-// build the nav
-
+// BUild Nav
 
 function build__nvUx() {
+    // Loop over sections to determine lis to build
     for (section of sections) {
+        // construct LI
         const nav__li = document.createElement("li");
-        nav__li.innerHTML = `<a href="#${section.id}">${section.id}</a>`;
+        // Insert html into LI
+        nav__li.innerHTML = `<a onclick="anch__scroll(${section.id})" href="#${section.id}">${section.id}</a>`;
+        // add classnames to LI
         nav__li.className = "navbar__menu li";
+        // append li to ul
         navbar.appendChild(nav__li);
-        console.log(nav__li);
-        console.log(section);
     };
-
     build__mnUx();
 };
+build__nvUx();
 
 
+function add__listeners() {
+    for (section of sections) {
+        const bounding = section.getBoundingClientRect();
+        console.log(bounding);
+        section.addEventListener("scroll", function () {
+            if (
+                bounding.top >= 0 &&
+                bounding.left >= 0 &&
+                bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+            ) {
+                section.className("active__class");
+            } else {
+                section.className(" ");
+            }
+        });
+    };
+};
 
-// Add class 'active' to section when near top of viewport
-
+add__listeners();
 
 // Scroll to anchor ID using scrollTO event
 
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu 
-
 function build__mnUx() {
-    navbar__head.className = "navbar__menu ul";
+    navbar__head.firstChild.className = "navbar__menu ul";
 
 }
 
-// Scroll to section on link click
+    // Scroll to section on link click
 
 // Set sections as active
-
-build__nvUx();
