2781a24194aaa6def577e52d9a362737f4d44bbc
MichAngel <mangelette@gmail.com>
2019-12-05 14:23:26 -0800

    Active Smooth Scroll. Viewport function

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Landing Page Project
+
+This project will add items to the navigation dynamically using Javascript. 
+
+Smooth scroll is to be active based on navigation link.
+Links will be highlighted based on what is in the active viewport. 
--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const navbar = document.getElementById("navbar__list");
 const navbar__head = document.getElementById("navbar__menu");
 const sections = document.querySelectorAll("section");
-
 // BUild Nav
 
 function build__nvUx() {
@@ -10,37 +9,56 @@ function build__nvUx() {
         // construct LI
         const nav__li = document.createElement("li");
         // Insert html into LI
-        nav__li.innerHTML = `<a onclick="anch__scroll(${section.id})" href="#${section.id}">${section.id}</a>`;
+        nav__li.innerHTML = `<a href="#${section.id}">${section.id}</a>`;
         // append li to ul
         navbar.appendChild(nav__li);
     };
 };
+
 build__nvUx();
 
-function anch__scroll() {
-    document.getElementById(section.id).scrollIntoView({
-        behavior: "smooth"
-    });
-    event.preventDefault();
-    console.log(section.id);
-};
+// This code is far more optimized and functions as a smooth scroll for all link tags. Script retrieved from https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
 
-function add__listeners() {
-    for (section of sections) {
-        const bounding = section.getBoundingClientRect();
-        console.log(bounding);
-        section.addEventListener("scroll", function () {
-            if (
-                bounding.top >= 0 &&
-                bounding.left >= 0 &&
-                bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-            ) {
-                section.className("active__class");
-            } else {
-                section.className(" ");
-            }
+
+// Grab all href elements
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        // Prevent default action . jump to section.
+        e.preventDefault();
+        // Select all elements (a tags) that require smooth scroll
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            // define scroll behavior
+            behavior: 'smooth'
         });
+    });
+});
+
+// Original Smooth Scroll function I wrote. 
+
+// function anch__scroll() {
+//     document.getElementById(this.section.id).scrollIntoView({
+//         behavior: "smooth"
+//     });
+// function needed to be assigned to each item when generating the section loop for this function to run for the smooth scroll.
+//     event.preventDefault();
+// };
+
+window.addEventListener("scroll", addClass)
+
+function addClass(event) {
+    var isInViewport = function (section) {
+        var bounding = section.getBoundingClientRect();
+        return (
+            bounding.top >= 0 &&
+            bounding.left >= 0 &&
+            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    };
+    if (isInViewport === true) {
+        section.classList.add("active__class");
+    } else {
+        section.classList.remove("active__class");
     };
 };
 
@@ -51,4 +69,3 @@ function build__mnUx() {
 }
 
 build__mnUx();
-add__listeners();
